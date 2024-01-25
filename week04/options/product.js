b0083078c822a212c8d520241d348c4c8cd1080c
dora <bigpowerpig@gmail.com>
2024-01-26 01:16:10 +0800

    week04-1-1

--- week04/options/product.js
+++ week04/options/product.js
@@ -5,8 +5,29 @@ const app = createApp({
       apiUrl: 'https://vue3-course-api.hexschool.io/v2',
       apiPath: 'dorayu',
       products: [],
-      tempProduct: {},
-
+      tempProduct: {
+        data: {
+          "title": "[賣]動物園造型衣服3",
+          "category": "衣服2",
+          "origin_price": 100,
+          "price": 300,
+          "unit": "個",
+          "description": "Sit down please 名設計師設計",
+          "content": "這是內容",
+          "is_enabled": 1,
+          "imageUrl": "主圖網址",
+          "imagesUrl": [
+            "圖片網址一",
+            "圖片網址二",
+            "圖片網址三",
+            "圖片網址四",
+            "圖片網址五"
+          ]
+        }
+      },
+      isNew: true,
+      bsProductModal: null,
+      bsDelProductModal: null,
     }
   },
   mounted() {
@@ -16,13 +37,22 @@ const app = createApp({
 
     this.checkLogin();
 
+    // bs modal 實體化
+    this.bsProductModal = new bootstrap.Modal(this.$refs.productModal, {
+      backdrop: 'static',
+      keyboard: false
+    });
+    this.bsDelProductModal = new bootstrap.Modal(this.$refs.delProductModal, {
+      backdrop: 'static',
+      keyboard: false
+    });
+
   },
   methods: {
     // 確認登入狀態
     checkLogin() {
       axios.post(`${this.apiUrl}/api/user/check`)
         .then(response => {
-          // console.log(response.data.success);
           this.getPorducts();
         })
         .catch(error => {
@@ -40,6 +70,22 @@ const app = createApp({
           alert(error.data.message)
         })
     },
+    // open bs modal 代值判斷傳入是哪一個
+    openProductModal(status, item) {
+      if(status === "createNew") {
+        this.isNew = true;
+        this.tempProduct.data = {};
+        this.bsProductModal.show();
+      } else if(status === "edit") {
+        this.isNew = false;
+        this.tempProduct.data = {...item};
+        this.bsProductModal.show();
+      } else if(status === "delete") {
+        this.tempProduct.data = {...item};
+        this.bsDelProductModal.show();
+      }
+      
+    }
   },
 });
 app.mount("#app");
